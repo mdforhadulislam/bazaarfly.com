6a679a32a732d9085f93446a924ce0637b54ef89
mdforhadulislam <mdforhadul44@gmail.com>
2026-02-07 18:38:15 +0600

    add alllll

--- src/server/middleware/auth.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest } from "next/server"; 
-import { parseUser } from "./parseUser";
-import { unauthorizedResponse } from "@/components/server/utils/response";
-
-export const auth = async (req: NextRequest) => {
-  const user = await parseUser(req);
-  if (!user) return unauthorizedResponse("Unauthorized");
-  return user;
-}; 
